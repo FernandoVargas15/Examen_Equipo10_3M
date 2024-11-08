c127a2b1e984605937286e157de526793f281eb9
FernandoVargas15 <luis.vargas89@unach.mx>
2024-11-07 19:10:29 -0600

    Agregar archivos de proyecto.

---
+++ Examen_Equipo10/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Examen_Equipo10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtFecha = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbTemperatura = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.estado = new System.Windows.Forms.Label();
+            this.btnEncender = new System.Windows.Forms.Button();
+            this.btnApagar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre de usuario:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fecha:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(193, 21);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 22);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // txtFecha
+            // 
+            this.txtFecha.Location = new System.Drawing.Point(93, 73);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.Size = new System.Drawing.Size(100, 22);
+            this.txtFecha.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Temperatura:";
+            // 
+            // lbTemperatura
+            // 
+            this.lbTemperatura.AutoSize = true;
+            this.lbTemperatura.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTemperatura.Location = new System.Drawing.Point(158, 133);
+            this.lbTemperatura.Name = "lbTemperatura";
+            this.lbTemperatura.Size = new System.Drawing.Size(37, 20);
+            this.lbTemperatura.TabIndex = 5;
+            this.lbTemperatura.Text = "0°C";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(555, 309);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(129, 64);
+            this.btnGuardar.TabIndex = 6;
+            this.btnGuardar.Text = "Guardar registro";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // estado
+            // 
+            this.estado.AutoSize = true;
+            this.estado.ForeColor = System.Drawing.Color.Red;
+            this.estado.Location = new System.Drawing.Point(372, 228);
+            this.estado.Name = "estado";
+            this.estado.Size = new System.Drawing.Size(64, 16);
+            this.estado.TabIndex = 7;
+            this.estado.Text = "Apagado";
+            // 
+            // btnEncender
+            // 
+            this.btnEncender.Location = new System.Drawing.Point(111, 309);
+            this.btnEncender.Name = "btnEncender";
+            this.btnEncender.Size = new System.Drawing.Size(129, 64);
+            this.btnEncender.TabIndex = 8;
+            this.btnEncender.Text = "Encender";
+            this.btnEncender.UseVisualStyleBackColor = true;
+            this.btnEncender.Click += new System.EventHandler(this.btnEncender_Click);
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.Location = new System.Drawing.Point(329, 309);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(129, 64);
+            this.btnApagar.TabIndex = 9;
+            this.btnApagar.Text = "Apagar";
+            this.btnApagar.UseVisualStyleBackColor = true;
+            this.btnApagar.Click += new System.EventHandler(this.btnApagar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(349, 197);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(129, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Estado del motor:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnApagar);
+            this.Controls.Add(this.btnEncender);
+            this.Controls.Add(this.estado);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.lbTemperatura);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtFecha);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtFecha;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbTemperatura;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label estado;
+        private System.Windows.Forms.Button btnEncender;
+        private System.Windows.Forms.Button btnApagar;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Examen_Equipo10/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient; 
+
+namespace Examen_Equipo10
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort puertoSerial;
+        private string ConectarSQL = "Server=localhost; Port=3306; Database=ExaPractica_04; Uid=root; Pwd=Rocket2014;";
+        private double temperaturaActual; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnApagar.Enabled = false;
+
+            
+            puertoSerial = new SerialPort("COM4", 9600); 
+            puertoSerial.DataReceived += new SerialDataReceivedEventHandler(DatosRecibidos);
+            puertoSerial.Open(); 
+        }
+
+        private void btnEncender_Click(object sender, EventArgs e)
+        {
+            if (puertoSerial.IsOpen)
+            {
+                puertoSerial.WriteLine("ENCENDER");
+                btnEncender.Enabled = false;
+                btnApagar.Enabled = true;
+
+                estado.ForeColor = Color.Green;
+                estado.Text = "Encendido"; 
+
+                MessageBox.Show("Comando para encender enviado.");
+            }
+        }
+
+        private void btnApagar_Click(object sender, EventArgs e)
+        {
+            if (puertoSerial.IsOpen)
+            {
+                puertoSerial.WriteLine("APAGAR");
+                btnApagar.Enabled = false;
+                btnEncender.Enabled = true;
+
+                estado.ForeColor = Color.Red;
+                estado.Text = "Apagado"; 
+
+                MessageBox.Show("Comando para apagar enviado.");
+            }
+        }
+
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string nombreUsuario = txtUsuario.Text; 
+            DateTime fechaRegistro;
+
+            if (DateTime.TryParse(txtFecha.Text, out fechaRegistro))
+            {
+                if (string.IsNullOrWhiteSpace(nombreUsuario))
+                {
+                    MessageBox.Show("Por favor ingresa un nombre de usuario.");
+                    return;
+                }
+
+                try
+                {
+                    using (MySqlConnection conexion = new MySqlConnection(ConectarSQL))
+                    {
+                        conexion.Open();
+
+                        string insertarUsuario = "INSERT INTO Usuarios (Nombre, FechaRegistro) VALUES (@Nombre, @Fecha)";
+                        MySqlCommand cmdUsuario = new MySqlCommand(insertarUsuario, conexion);
+                        cmdUsuario.Parameters.AddWithValue("@Nombre", nombreUsuario);
+                        cmdUsuario.Parameters.AddWithValue("@Fecha", fechaRegistro);
+                        cmdUsuario.ExecuteNonQuery();
+
+                        int usuarioId = (int)cmdUsuario.LastInsertedId; 
+
+                        string insertarLectura = "INSERT INTO Lecturas (UsuarioId, Temperatura, FechaHora) VALUES (@UsuarioId, @Temperatura, @FechaHora)";
+                        MySqlCommand cmdLectura = new MySqlCommand(insertarLectura, conexion);
+                        cmdLectura.Parameters.AddWithValue("@UsuarioId", usuarioId);
+                        cmdLectura.Parameters.AddWithValue("@Temperatura", temperaturaActual);
+                        cmdLectura.Parameters.AddWithValue("@FechaHora", DateTime.Now);
+                        cmdLectura.ExecuteNonQuery();
+
+                        MessageBox.Show("Datos guardados correctamente.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al guardar los datos: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("La fecha no es válida.");
+            }
+        }
+
+        private void DatosRecibidos(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = puertoSerial.ReadLine(); 
+
+            if (data.StartsWith("Temperatura (C):"))
+            {
+
+                string tempString = data.Substring("Temperatura (C):".Length).Trim();
+                double temperatura;
+                if (double.TryParse(tempString, out temperatura))
+                {
+                    temperaturaActual = temperatura; 
+                    this.Invoke(new Action(() => lbTemperatura.Text = temperatura.ToString("F1") + " °C"));
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (puertoSerial != null && puertoSerial.IsOpen)
+            {
+                puertoSerial.Close();
+            }
+        }
+    }
+}
